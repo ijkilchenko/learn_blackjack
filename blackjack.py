72673fa07d268cc5502eda500d7fd879d7972b6e
Alex Ilchenko <ijkilchenko@gmail.com>
2021-12-23 19:58:10 -0500

    Add function to find the highest possible value.

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,16 @@
 import random
+from enum import Enum
+from typing import List
+
+
+class Move(Enum):
+  BLACKJACK = 1
+  BUST = 2
+  HIT = 3
+  STAND = 4
+  DOUBLE = 5
+  SPLIT = 6
+  # TODO: Maybe add insurance and surrender?
 
 
 class Runner:
@@ -13,9 +25,10 @@ class Runner:
 class Game:
   CARDS = '23456789TJQKA'
   CARD_2_VAL = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
-                'T': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
+                'T': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 1}
+  BLACKJACK_VAL = 21
 
-  def __init__(self, num_of_decks=6):
+  def __init__(self, num_of_decks=6, blackjack_pays=3 / 2):
     self.num_of_decks = num_of_decks
     self.shoe = self.CARDS * num_of_decks
     self.shoe = random.sample(self.shoe, len(self.shoe))
@@ -27,9 +40,9 @@ class Game:
     self.count = 0
 
     # Blackjack variations properties
-    self.blackjack_pays = 3 / 2  # Multiplier, 3 to 2
+    self.blackjack_pays = blackjack_pays  # Multiplier, e.g. 3:2
 
-  def deal_card(self) -> str:
+  def _deal_card(self) -> str:
     """Function uses the current shoe to get the next card.
     Puts the card into the discard_pile."""
 
@@ -37,15 +50,22 @@ class Game:
     self.discard_pile.append(card)
     return card
 
-  def deal(self):
+  def deal_hand(self):
     # Clear previous hands.
     self.player = Player()
     self.dealer = Dealer()
 
-    self.player.cards.append(self.deal_card())  # Up
-    self.dealer.cards.append(self.deal_card())  # Down
-    self.player.cards.append(self.deal_card())  # Up
-    self.dealer.cards.append(self.deal_card())  # Up
+    self.player.cards.append(self._deal_card())  # Up
+    self.dealer.cards.append(self._deal_card())  # Down
+    self.player.cards.append(self._deal_card())  # Up
+    self.dealer.cards.append(self._deal_card())  # Up
+
+  def get_allowed_moves_for_hand(self) -> List[Move]:
+    if self.player.has_blackjack():
+      return [Move.BLACKJACK]
+
+  def play_hand(self, move):
+    pass
 
 
 class Player:
@@ -53,6 +73,23 @@ class Player:
 
   def __init__(self, cards=[]):
     self.cards = cards
+
+  def find_highest_val_before_bust(self):
+    val = sum([Game.CARD_2_VAL[card] for card in self.cards])
+    num_aces = self.cards.count('A')
+    for _ in range(num_aces):
+      if val + 10 <= Game.BLACKJACK_VAL:
+        val = val + 10  # Count this ace as 11
+      else:
+        break
+    return val
+
+  def has_blackjack(self):
+    val = self.find_highest_val_before_bust()
+    if val == Game.BLACKJACK_VAL:
+      return True
+    else:
+      return False
 
 
 class Dealer:
--- blackjack_test.py
+++ blackjack_test.py
@@ -23,9 +23,48 @@ class GameTest(unittest.TestCase):
   def test_deal_card(self):
     game1 = Game(num_of_decks=1)
     for _ in range(self.NUM_CARDS_IN_DECK):
-      game1.deal_card()
+      game1._deal_card()
     self.assertEqual(len(game1.shoe), 0)
     self.assertEqual(len(game1.discard_pile), self.NUM_CARDS_IN_DECK)
+
+  def test_find_highest_val_before_bust(self):
+    game1 = Game()
+
+    game1.player.cards = ['2', 'J']  # 12
+    self.assertEqual(game1.player.find_highest_val_before_bust(), 12)
+
+    game1.player.cards = []  # 0
+    self.assertEqual(game1.player.find_highest_val_before_bust(), 0)
+
+    game1.player.cards = ['A', '2']  # 13
+    self.assertEqual(game1.player.find_highest_val_before_bust(), 13)
+
+    game1.player.cards = ['A', '2', 'A']  # 14
+    self.assertEqual(game1.player.find_highest_val_before_bust(), 14)
+
+  def test_has_blackjack(self):
+    game1 = Game()
+
+    game1.player.cards = ['A', 'J']  # 21
+    self.assertTrue(game1.player.has_blackjack())
+
+    game1.player.cards = ['A', 'T']  # 21
+    self.assertTrue(game1.player.has_blackjack())
+
+    game1.player.cards = ['2', 'J']  # 12
+    self.assertFalse(game1.player.has_blackjack())
+
+    game1.player.cards = ['2', 'J', '9']  # 21
+    self.assertTrue(game1.player.has_blackjack())
+
+    game1.player.cards = ['2', 'J', '9']  # 21
+    self.assertTrue(game1.player.has_blackjack())
+
+    game1.player.cards = []  # 0
+    self.assertFalse(game1.player.has_blackjack())
+
+    game1.player.cards = ['A', '2']  # 13
+    self.assertFalse(game1.player.has_blackjack())
 
 
 if __name__ == '__main__':
